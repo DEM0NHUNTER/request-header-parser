30155ca36e2267d941b40297a8f6c941b4d10cfa
Krieg <potatoclan0@gmail.com>
2025-07-08 00:54:31 +0300

    Profile fixes

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Core route
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress, language, software });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
