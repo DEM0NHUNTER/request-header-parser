61ff71facf37a7f5f19a8af950c997939ad0069e
Krieg <potatoclan0@gmail.com>
2025-07-08 01:03:43 +0300

    Fixes

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Core route
-app.get('/api/whoami', (req, res) => {
-  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0] || req.connection.remoteAddress;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-
-  res.json({
-    ipaddress,
-    language,
-    software
-  });
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for']?.split(',')[0]
+                  || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
+});
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
